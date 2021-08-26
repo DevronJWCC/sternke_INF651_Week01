7b73ec49b7228c0dc44067b7a105fc6b07cf5e28
DevronJWCC <89314794+DevronJWCC@users.noreply.github.com>
2021-08-26 16:14:04 -0500

    Supercharging our example website

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,3 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World!';
-
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
@@ -11,3 +8,28 @@ myImage.onclick = function() {
         myImage.setAttribute('src','images/firefox-icon.png');
     }
 }
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
+  
